c3664ff79893145e77aae6f79cb253c42d6e1d03
Edison0341 <edison0341@gmail.com>
2024-11-15 08:41:48 -0400

    first push and README

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+export default class ClassRoom {
+    /**
+     * @param {number} maxStudentsSize
+     */
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
