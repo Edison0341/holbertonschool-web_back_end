6852ea444b2412b03e0b3a472e84ab3b8148a55f
Edison0341 <edison0341@gmail.com>
2024-12-06 12:32:52 -0400

    README & Task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Type annotated function (add) that takes two float arguments (a, b),
+    and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Adds two floating-point numbers """
+    return a + b
